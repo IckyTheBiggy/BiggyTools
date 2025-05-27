d02b5559c4a907b7bfa0233774c566701377d322
IckyTheBiggy <elcia116eb@gmail.com>
2025-05-27 15:17:46 +0100

    ImGui Window

---
+++ BiggyTools.Rendering/UIWindow.cs
@@ -0,0 +1,69 @@
+using ImGuiNET;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Rendering.UI
+{
+    public class UIWindow : GameWindow
+    {
+        private ImGuiController _imGuiController;
+
+        public UIWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            
+        }
+
+        private void RenderUI()
+        {
+            ImGui.Begin("Biggy Tools");
+            ImGui.Text("BiggyTools");
+            ImGui.End();
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+            _imGuiController = new ImGuiController(ClientSize.X, ClientSize.Y);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            _imGuiController.Update(this, (float)args.Time);
+
+            RenderUI();
+
+            _imGuiController.Render();
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, ClientSize.X, ClientSize.Y);
+            _imGuiController.WindowResized(ClientSize.X, ClientSize.Y);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            _imGuiController.Dispose();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿using Spectre.Console;
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using Rendering.UI;
+using Spectre.Console;
 
 namespace BiggyTools
 {
     class Program
     {
+        static NativeWindowSettings nativeWindowSettings = new NativeWindowSettings
+        {
+            ClientSize = new Vector2i(800, 600),
+            Title = "BiggyTools"
+        };
+
         public static void Main(string[] args)
         {
             var option = AnsiConsole.Prompt(
@@ -12,6 +21,7 @@ namespace BiggyTools
                 .PageSize(10)
                 .AddChoices(new[] {
                     "Re-Encode Video",
+                    "UI",
                     "Exit"
                 }));
 
@@ -19,6 +29,11 @@ namespace BiggyTools
             {
                 case "Re-Encode Video":
                     FFmpeg.Utils.StartRencode();
+                    break;
+
+                case "UI":
+                    UIWindow uIWindow = new UIWindow(GameWindowSettings.Default, nativeWindowSettings);
+                    uIWindow.Run();
                     break;
             }
         }
