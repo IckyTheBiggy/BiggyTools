efbef45549abf8e94b79145a86a2fe1db636a2ee
IckyTheBiggy <elcia116eb@gmail.com>
2025-05-29 22:00:09 +0100

    Changed buildscript command

--- installLinux.sh
+++ installLinux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-dotnet publish -c Release -r linux-x64 -p:PublishAot=true
+dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true -p:IncludeNativeLibrariesForSelfExtract=true
 sudo cp bin/Release/net9.0/linux-x64/native/BiggyTools /usr/bin/biggytools
